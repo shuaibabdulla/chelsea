ae7628cb931d742b4839507498a1f703e26ad238
Shuaib Kunhabdulla <shuaibabdulla40@gmail.com>
2021-06-11 09:32:55 +0400

    Setup
    Projet setp

---
+++ src/main/java/com/football/fan/chelsea/ChelseaApplication.java
@@ -0,0 +1,13 @@
+package com.football.fan.chelsea;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChelseaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChelseaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/football/fan/chelsea/ChelseaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.football.fan.chelsea;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChelseaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
